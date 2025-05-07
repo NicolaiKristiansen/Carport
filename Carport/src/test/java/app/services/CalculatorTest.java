f2437cfa8909b694205e01623225fcd8e7394d09
sofusk7cdk <sofus@k7c.dk>
2025-05-07 09:55:23 +0200

    Test fixed with new database

--- Carport/src/test/java/app/services/CalculatorTest.java
+++ Carport/src/test/java/app/services/CalculatorTest.java
@@ -8,8 +8,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalculatorTest {
 
     private static final String USER = "postgres";
-    private static final String PASSWORD = "postgres";
-    private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
+    private static final String PASSWORD = System.getenv("password");
+    private static final String URL = "jdbc:postgresql://" + System.getenv("ip") + ":5432/%s?currentSchema=public";
     private static final String DB = "Carport";
 
     private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
