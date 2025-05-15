79c837d43de26c7dcf70f5976800722ee0b4f4d1
sofusk7cdk <sofus@k7c.dk>
2025-05-15 10:05:49 +0200

    Calculate price

--- Carport/src/main/java/app/Main.java
+++ Carport/src/main/java/app/Main.java
@@ -38,7 +38,7 @@ public class Main {
     private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
 
     public static void main(String[] args) {
-        //Message to commit and updateeeee
+        //Message to commit and updateeeeee
 
         // mail method
         //MailUtil mailUtil = new MailUtil();
